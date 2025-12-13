dae256b07526d4ec1f1298cf7567b78579b0512e
Dietrich Dehlinger <ddehling@gmail.com>
2025-12-12 16:11:44 -0800

    initial wave terrain method

--- Stories_OGL.py
+++ Stories_OGL.py
@@ -401,7 +401,7 @@ if __name__ == "__main__":
     # Start with summer bloom weather
     env_system.transition_to_weather(WeatherState.FIREFLY)
     #env_system.scheduler.schedule_event(0, 300, fx.shader_aurora,frame_id=0)  # noqa: F405
-    env_system.scheduler.schedule_event(0, 60, fx.shader_voronoi_sphere,frame_id=0)
+    env_system.scheduler.schedule_event(0, 90, fx.shader_wave_terrain,frame_id=0)
     #env_system.scheduler.schedule_event(10, 20, fx.shader_gameoflife,frame_id=0)  # noqa: F405
     #env_system.scheduler.schedule_event(0, 500, fx.shader_meteor,frame_id=0)
     last_time = time.time()
